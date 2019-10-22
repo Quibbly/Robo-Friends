a76cf7b54eaa758ae7c439583b29ab84ad6eb32e
Quibbly <owenhedwards@gmail.com>
2019-10-22 14:13:45 +0100

    Updates

---
+++ precache-manifest.dd41d226af0f62964ae3cc8adaa5c1c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eee5b4cbc6cd2c62f70ccd425acd9c3",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/Robo-Friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8fd842afb11f739e19df",
+    "url": "/Robo-Friends/static/css/main.47f6eceb.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/Robo-Friends/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "8fd842afb11f739e19df",
+    "url": "/Robo-Friends/static/js/main.36cf9f42.chunk.js"
+  },
+  {
+    "revision": "0e7758a9b59a84f63eed",
+    "url": "/Robo-Friends/static/js/runtime-main.c2d9c9a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
